cac7e8d20351d05b31b2c3c0a0d3c6c9a6876ee0
yixihan <yixihan20010617@gmail.com>
2023-04-02 01:09:03 +0800

    用户模块-设置 接入后端

--- src/views/userCenter/Setting.vue
+++ src/views/userCenter/Setting.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="setting">
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="个人信息修改" name="first">
+    <el-tabs v-model="activeName">
+      <!-- 用户个人信息设置 -->
+      <el-tab-pane label="个人信息修改" name="info">
         <el-form label-position="right" label-width="150px" :model="userInfo">
-          <el-form-item label="用户名">
-            <el-input v-model="userInfo.username"></el-input>
-          </el-form-item>
-          <el-form-item label="用户邮箱">
-            <el-input v-model="userInfo.email"></el-input>
-          </el-form-item>
-          <el-form-item label="用户手机号">
-            <el-input v-model="userInfo.phone"></el-input>
-          </el-form-item>
           <el-form-item label="用户头像">
             <el-upload
                 action="https://yicode.oss-cn-chengdu.aliyuncs.com"
@@ -24,48 +16,56 @@
                 :before-upload="beforeUpload"
                 :on-success="handleUploadSuccess"
             >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <img v-if="userInfo.userAvatar"
+                   :src="userInfo.userAvatar"
+                   alt="正在加载中"
+                   class="avatar"
+              >
               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
           </el-form-item>
-          <el-form-item label="用户省份">
-            <el-input v-model="userInfo.province"></el-input>
-          </el-form-item>
-          <el-form-item label="用户城市">
-            <el-input v-model="userInfo.city"></el-input>
+          <el-form-item label="用户所在地">
+            <el-cascader
+                size="large"
+                :options="options"
+                v-model="selectedOptions"
+                @change="changeCity">
+            </el-cascader>
           </el-form-item>
           <el-form-item label="用户性别">
-            <el-radio v-model="userInfo.sex" label="1">男</el-radio>
-            <el-radio v-model="userInfo.sex" label="2">女</el-radio>
+            <el-radio v-model="userInfo.userSex" label="MAN">男</el-radio>
+            <el-radio v-model="userInfo.userSex" label="WOMAN">女</el-radio>
+            <el-radio v-model="userInfo.userSex" label="SECRECY">保密</el-radio>
           </el-form-item>
           <el-form-item label="用户签名">
-            <el-input v-model="userInfo.sign"></el-input>
+            <el-input v-model="userInfo.userSign"></el-input>
           </el-form-item>
           <el-form-item label="用户学校">
-            <el-input v-model="userInfo.school"></el-input>
+            <el-input v-model="userInfo.userSchool"></el-input>
           </el-form-item>
           <el-form-item label="用户生日">
             <el-date-picker
-                v-model="userInfo.birthday"
+                v-model="userInfo.userBirth"
                 type="date"
                 placeholder="选择日期">
             </el-date-picker>
           </el-form-item>
           <el-form-item label="用户真实姓名">
-            <el-input v-model="userInfo.name"></el-input>
+            <el-input v-model="userInfo.userRealName"></el-input>
           </el-form-item>
           <el-form-item label="用户个人网站">
-            <el-input v-for="(item, index) in userInfo.websites"
-                      :key="index"
-                      v-model="item.path"></el-input>
-            <el-button @click="addWeb()">添加</el-button>
+              <el-input v-for="(item, index) in websiteList"
+                        :key="index"
+                        v-model="item.value"></el-input>
+            <el-button @click="modifyWebsite()">添加</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit()">修改</el-button>
+            <el-button type="primary" @click="modifyUserInfo()">修改</el-button>
           </el-form-item>
         </el-form>
       </el-tab-pane>
-      <el-tab-pane label="邮箱修改" name="second">
+      <!-- 用户邮箱设置 -->
+      <el-tab-pane label="邮箱修改" name="email">
         <el-form label-position="right" label-width="150px" :model="emailModify">
           <el-form-item label="用户邮箱">
             <el-input v-model="emailModify.email"></el-input>
@@ -79,7 +79,8 @@
           </el-form-item>
         </el-form>
       </el-tab-pane>
-      <el-tab-pane label="手机号修改" name="third">
+      <!-- 用户手机号设置 -->
+      <el-tab-pane label="手机号修改" name="mobile">
         <el-form label-position="right" label-width="150px" :model="phoneModify">
           <el-form-item label="用户手机号" width="300px">
             <el-input v-model="phoneModify.phone"></el-input>
@@ -93,8 +94,9 @@
           </el-form-item>
         </el-form>
       </el-tab-pane>
-      <el-tab-pane label="密码修改" name="fourth">
-        <el-form label-position="right" label-width="150px" :model="formLabelAlign">
+      <!-- 用户密码设置 -->
+      <el-tab-pane label="密码修改" name="password">
+        <el-form label-position="right" label-width="150px" :model="pwdModify">
           <el-form-item label="手机号/邮箱">
             <el-input v-model="pwdModify.account"></el-input>
           </el-form-item>
@@ -113,52 +115,74 @@
           </el-form-item>
         </el-form>
       </el-tab-pane>
+      <!-- 用户用户名设置 -->
+      <el-tab-pane label="用户名修改" name="username">
+        <el-form label-position="right" label-width="150px" :model="nameModify">
+          <el-form-item label="用户名">
+            <el-input v-model="nameModify.userName"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="modifyUserName()">修改</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
 import {uuid} from "@/util/uuidUtil";
+import {successMsg} from "@/util/elementMsgUtil";
+import { provinceAndCityData } from 'element-china-area-data'
 
 export default {
   name: "Setting",
+  inject: ['reload'],
   data () {
     return {
-      activeName: 'first',
+      activeName: 'info',
+      // 用户信息
       userInfo: {
-        username: '',
-        email: '',
-        phone: '',
-        avatar: '',
-        province: '',
-        city: '',
-        sex:'1',
-        sign: '',
-        school: '',
-        birthday: '',
-        name:  '',
-        websites: [
-          {
-            name: '',
-            path: '',
-          }
-        ],
+        userId: 0,
+        userAvatar: '',
+        userBirth: '',
+        userCity: '',
+        userProvince: '',
+        userRealName: '',
+        userSchool: '',
+        userSex:'1',
+        userSign: '',
+        userWebsiteList: [],
       },
+      // 用户网站
+      websiteList: [
+        {
+          name: '',
+          value: ''
+        }
+      ],
+      // 邮箱修改
       emailModify: {
         email: '',
         verificationCode: '',
       },
+      // 手机号修改
       phoneModify: {
         phone: '',
         verificationCode: '',
       },
+      // 密码修改
       pwdModify: {
         account: '',
         pwd: '',
         pwdSecond: '',
         verificationCode: '',
       },
-      imageUrl: '',
+      // 用户名修改
+      nameModify: {
+        userId: this.$store.getters.getUserId,
+        userName: this.$store.getters.getUser.userName
+      },
       // 图片上传秘钥
       dataObj: {
         policy: "",
@@ -168,6 +192,9 @@ export default {
         dir: "",
         host: "",
       },
+      // 地图 options
+      options: provinceAndCityData,
+      selectedOptions: []
     }
   },
   computed: {
@@ -193,22 +220,91 @@ export default {
       ];
     },
   },
+  created() {
+    this.userInfo = this.$store.getters.getUserInfo
+    this.selectedOptions.push(this.userInfo.userProvince)
+    this.selectedOptions.push(this.userInfo.userCity)
+    this.initUserWebsite()
+  },
   methods: {
     // 添加网站
-    addWeb () {
-      this.userInfo.websites.push({
+    modifyWebsite () {
+      this.websiteList.push({
         name: '',
-        path: ''
+        value: ''
       })
     },
-    handleClick(tab, event) {
-      console.log(tab, event);
+    // 获取用户信息
+    getUserInfo() {
+      this.asyncGetLoginUserInfo().then(({data}) => {
+        this.$store.commit("setUserInfo", data.data)
+        this.reload()
+      })
+    },
+    // 修改用户信息
+    modifyUserInfo() {
+      // 同步用户网站
+      this.dealUserWebsite()
+      this.asyncModifyUserInfo().then(() => {
+        successMsg("用户信息修改成功")
+        this.getUserInfo()
+      })
+
+    },
+    // 绑定邮箱
+    bindUserEmail() {
+
+    },
+    // 解绑邮箱
+    unbindUserEmail() {
+
+    },
+    // 绑定手机号
+    bindUserMobile() {
+
+    },
+    // 解绑手机号
+    unbindUserMobile() {
+
+    },
+    // 修改密码
+    modifyUserPassword() {
+
+    },
+    // 修改用户名
+    modifyUserName() {
+      this.asyncModifyUserName().then(() => {
+        successMsg("用户名修改成功")
+        this.getUserInfo()
+      })
     },
     // 发送验证码
     sendCode () {
       // 根据activeName定位
     },
     onSubmit () {},
+    // 更新 => 更改用户所在地
+    changeCity (value) {
+      this.userInfo.userProvince = value[0]
+      this.userInfo.userCity = value[1]
+    },
+    // 初始化 => 同步用户网站
+    initUserWebsite() {
+      this.websiteList = []
+      this.userInfo.userWebsiteList.forEach((item) => {
+        this.websiteList.push({
+          name: '',
+          value: item
+        })
+      })
+    },
+    // 更新 => 同步用户网站
+    dealUserWebsite() {
+      this.userInfo.userWebsiteList = []
+      this.websiteList.forEach((item) => {
+        this.userInfo.userWebsiteList.push(item.value)
+      })
+    },
     // 上传前钩子 => 获取 oss 上传凭证
     beforeUpload() {
       return new Promise((resolve) => {
@@ -236,13 +332,178 @@ export default {
     handleUploadSuccess(res, file) {
       this.fileList.pop();
       this.fileList.push({
-        questionName: file.name,
+        name: file.name,
         url:
             this.dataObj.host +
             "/" +
             this.dataObj.key.replace("${filename}", file.name),
       });
-      this.questionListForm.questionListBg = this.fileList[0].url;
+      this.userInfo.userAvatar = this.fileList[0].url;
+    },
+    // 异步方法 => 获取登录用户信息
+    async asyncGetLoginUserInfo() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/now",
+        method: "get",
+      });
+    },
+    // 异步方法 => 绑定邮箱
+    async asyncBindUserEmail() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/bind/email",
+        method: "post",
+        data: {
+          "email": "a.ecpwol@qq.com",
+          "code": "21"
+        }
+      });
+    },
+    // 异步方法 => 解绑邮箱
+    async asyncUnbindUserEmail() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/info/modify",
+        method: "post",
+        data: {
+          "email": "a.ecpwol@qq.com",
+          "code": "21"
+        }
+      });
+    },
+    // 异步方法 => 绑定手机号
+    async asyncBindUserMobile() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/bind/mobile",
+        method: "post",
+        data: {
+          "mobile": "18133820517",
+          "code": "44"
+        }
+      });
+    },
+    // 异步方法 => 解绑手机号
+    async asyncUnbindUserMobile() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/info/modify",
+        method: "post",
+        data: {
+          "mobile": "18133820517",
+          "code": "44"
+        }
+      });
+    },
+    // 异步方法 => 修改用户密码
+    async asyncModifyUserPassword() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/info/modify",
+        method: "post",
+        data: {
+          "email": "r.lkmnxi@qq.com",
+          "userId": 42,
+          "mobile": "18651714424",
+          "newPassword": "quis dolore do",
+          "type": "laborum"
+        }
+      });
+    },
+    // 异步方法 => 修改用户用户名
+    async asyncModifyUserInfo() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/info/modify",
+        method: "post",
+        data: this.userInfo
+      });
+    },
+    // 异步方法 => 修改用户用户名
+    async asyncModifyUserName() {
+      return await this.$axios({
+        url: "/yicode-user-openapi/open/user/reset/username",
+        method: "post",
+        data: this.nameModify
+      });
+    },
+    // 异步方法 => 发送邮件
+    async asyncSendCommonEmail() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/email/send/email/common",
+        method: "post",
+        data: {
+          "email": ''
+        }
+      });
+    },
+    // 异步方法 => 校验邮箱验证码
+    async asyncValidateCommonEmailCode() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/email/validate/common",
+        method: "post",
+        data: {
+          "email": '',
+          "code": ''
+        }
+      });
+    },
+    // 异步方法 => 发送短信
+    async asyncSendCommonMobile() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/sms/send/mobile/common",
+        method: "post",
+        data: {
+          "mobile": ''
+        }
+      });
+    },
+    // 异步方法 => 校验短信验证码
+    async asyncValidateCommonMobileCode() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/sms/validate/common",
+        method: "post",
+        data: {
+          "mobile": '',
+          "code": ''
+        }
+      });
+    },
+    // 异步方法 => 发送邮件
+    async asyncSendResetEmail() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/email/send/email/reset",
+        method: "post",
+        data: {
+          "email": ''
+        }
+      });
+    },
+    // 异步方法 => 校验邮箱验证码
+    async asyncValidateResetEmailCode() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/email/validate/reset",
+        method: "post",
+        data: {
+          "email": '',
+          "code": ''
+        }
+      });
+    },
+    // 异步方法 => 发送短信
+    async asyncSendResetMobile() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/sms/send/mobile/reset",
+        method: "post",
+        data: {
+          "mobile": ''
+        }
+      });
+    },
+    // 异步方法 => 校验短信验证码
+    async asyncValidateResetMobileCode() {
+      return await this.$axios({
+        url: "/yicode-thirdpart-openapi/open/sms/validate/reset",
+        method: "post",
+        data: {
+          "mobile": '',
+          "code": ''
+        }
+      });
     },
   }
 }
@@ -296,6 +557,9 @@ export default {
       width: 178px;
       height: 178px;
       display: block;
+    }
+    .el-cascader {
+      display: flex;
     }
   }
 
